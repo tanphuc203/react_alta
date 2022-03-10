4efa08647ef68f01f6664d6eaf5292f00074391c
tanphuc203 <hotanphuc832@gmail.com>
2022-03-10 13:04:28 +0700

    update 10/03/2022

--- src/component/Addservices/index.js
+++ src/component/Addservices/index.js
@@ -39,7 +39,52 @@ const index = () => {
               <p>Mô tả: </p>
               <input className="input2" placeholder="Mô tả dịch vụ"></input>
             </div>
-            <div className="addservices_input_bottom">Quy tắc cấp số</div>
+            <div className="bottom">
+              <div className="addservices_bottom">Quy tắc cấp số</div>
+              <div className="addservices_bottom_checkbox">
+                <div className="addservices_bottom_checkbox_items">
+                  <div className="addservices_bottom_checkbox_item">
+                    <input type="checkbox" />
+                    <span>Tăng tự động từ:</span>
+                  </div>
+                  <div className="addservices_bottom_checkbox_item">
+                    <input type="checkbox" name="" value="" />
+                    <span>Prefix:</span>
+                  </div>
+                  <div className="addservices_bottom_checkbox_item">
+                    <input type="checkbox" />
+                    <span>Surfix:</span>
+                  </div>
+                  <div className="addservices_bottom_checkbox_item">
+                    <input type="checkbox" />
+                    <span>Reset mỗi ngày</span>
+                  </div>
+                </div>
+                <div className="addservices_bottom_input_items">
+                  <div className="addservices_bottom_input_item_item">
+                    <input type="reset" name="" value="0001" />
+                    <span>đến</span>
+                    <input type="reset" name="" value="9999" />
+                  </div>
+                  <div className="addservices_bottom_input_item_item">
+                    <input type="reset" name="" value="0001" />
+                  </div>
+
+                  <div className="addservices_bottom_input_item_item">
+                    <input type="reset" name="" value="0001" />
+                  </div>
+                </div>
+              </div>
+              <div className="bottom_content">
+              <div className="bottom_content_about">
+                <p className="">*</p>
+                <span className="">
+                  Là trường thông tin bắt buộc
+                </span>
+              </div>
+            </div>
+            </div>
+            
           </div>
           <div className="addservices_input_button">
             <Link to="/Service">
